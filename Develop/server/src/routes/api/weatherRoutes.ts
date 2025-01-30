e25b7ecc00e5bd485eec1135e87564318766c22c
Alyssa <alyssarichards3@gmail.com>
2025-01-30 14:33:15 -0700

    more changes

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,27 +1,37 @@
-import { Router, type Request, type Response } from 'express';
+import { Router } from 'express';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
+
 import WeatherService from '../../service/weatherService.js';
 
-// TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
-    try {
-    // TODO: GET weather data from city name
-      const cityName = req.body.cityName;
-  
-      WeatherService.getWeatherForCity(cityName).then((data) => {
-        // TODO: save city to search history
-        // HistoryService.addCity(cityName);
-  
-        res.json(data);
-      });
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+// import WeatherService from '../../service/weatherService.js';
 
-// // * BONUS TODO: DELETE city from search history
-// router.delete('/history/:id', async (req, res) => {});
+// xTODO: POST Request with city name to retrieve weather data
+router.post('/', (req, res) => {
+  try {
+    // xTODO: save city to search history
+    const cityName = req.body.cityName;
+    WeatherService.getWeatherForCity(cityName).then((data) => {
+      HistoryService.addCity(cityName);
+
+      res.json(data);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// x TODO: GET search history
+router.get('/history', async (_req: Request, res: Response) => {
+  HistoryService.getCities()
+    .then((data) => {
+      return res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/history/:id', async (req, res) => { });
 
 export default router;
